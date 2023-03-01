6d94258fc30705afa4b54f4523632f7cfddca475
NirmalGundlapelly <nirmalgoud2103@gmail.com>
2023-03-01 17:02:01 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'
+import Home from './components/Home'
+
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login/>}/>
-          </Routes>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import Header from './Header';
+
+const Home = (props) => {
+
+    return <div>
+        <Header/>
+        Home
+        </div>
+}
+
+export default Home;
