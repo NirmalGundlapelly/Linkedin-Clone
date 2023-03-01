dc316c1aceb3bb674b783cbddc79c6dd4fc5bf2b
NirmalGundlapelly <nirmalgoud2103@gmail.com>
2023-03-01 20:15:00 +0530

    initial

---
+++ src/components/Rightside.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Righside = (props) => {
+  return <Container>Leftside</Container>;
+};
+
+const Container = styled.div`
+  grid-area: Rightside;
+`;
+
+export default Righside;
